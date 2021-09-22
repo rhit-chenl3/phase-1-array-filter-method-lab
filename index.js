4b6cc83757343810338d6a60d68bcd122903911e
Leo Chen <chenl3@rose-hulman.edu>
2021-09-22 17:53:25 -0400

    commit tree b27abcea40cf5bafdfd69b94ad526797baaa8c8a

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(driverNames, nameMatch){
+    return driverNames.filter(name => name.toUpperCase() === nameMatch.toUpperCase());
+}
+
+function fuzzyMatch(driverNames, nameBegin){
+    return driverNames.filter(name => name.startsWith(nameBegin));
+}
+
+function matchName(drivers, nameMatch){
+    return drivers.filter(driver => driver.name === nameMatch);
+}
